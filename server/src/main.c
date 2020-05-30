f9b62832759772e14f3f6e349810665edccdba33
Guillaume Bonnet <guillaume.bonnet@epitech.eu>
2020-05-31 01:50:13 +0200

    Add help

--- server/src/main.c
+++ server/src/main.c
@@ -6,27 +6,42 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "def/code.h"
 #include "server/server.h"
 #include "server/server_load.h"
 #include "server/server_save.h"
 
-int main(void)
+void help(const char *binary)
 {
-    setbuf(stdout, NULL); // TODO: Debug
+    printf(
+        "USAGE: %s port"
+        "\n\n",
+        binary);
+    printf(
+        "\t"
+        "port"
+        "\t"
+        "is the port number on which the server socket listens.\n");
+}
 
-    server_t *server = server_create(4242);
+int main(int argc, char **argv)
+{
+    if ((argc < 2) || (strcmp(argv[1], "-help") == 0)) {
+        help(argv[0]);
+        return (CODE_SUCCESS);
+    }
+
+    int port = atoi(argv[1]);
+    server_t *server = server_create(port);
 
     server_load(server);
-    if (server_init(server) == CODE_ERROR) {
-        perror("server_init"); // TODO: Debug
+    if (server_init(server) == CODE_ERROR)
         return (CODE_ERROR);
-    }
-    if (server_run(server) == CODE_ERROR) {
-        perror("server_run"); // TODO: Debug
+    if (server_run(server) == CODE_ERROR)
         return (CODE_ERROR);
-    }
     server_save(server);
     server_delete(server);
     return (0);
